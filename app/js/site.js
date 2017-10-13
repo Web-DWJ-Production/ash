d7fae7791fda982ff94076a245dee1bc58d7f37d
reddingk <kris.redding3@gmail.com>
2017-10-12 23:34:41 -0400

    add carousel, clean up design, update content

---
+++ app/js/site.js
@@ -0,0 +1,54 @@
+var carousel = new CeruleanCarousel(document.getElementsByClassName('carousel-item'), 10000, adjust);
+
+carousel.start();
+
+function adjust(me) {
+    var carouselBtns = document.getElementsByClassName('carousel-btn');
+    var carouselContent = document.getElementsByClassName('switch-content');
+
+    var outgoingId = me.currId;
+    var incomingId = (me.currId == (me.members.length - 1)) ? 0 : (me.currId + 1);
+
+    carouselBtns[outgoingId].classList.remove('active-btn');
+    carouselContent[outgoingId].classList.remove('active');
+
+    carouselBtns[incomingId].classList.add('active-btn');
+    carouselContent[incomingId].classList.add('active');
+}
+
+function go(id) {
+    var carouselBtns = document.getElementsByClassName('carousel-btn');
+    var carouselContent = document.getElementsByClassName('switch-content');
+    
+    for (var i = 0; i < carouselBtns.length; i++) {
+        carouselBtns[i].classList.remove('active-btn');
+        carouselContent[i].classList.remove('active');
+    }
+
+    carouselBtns[id].classList.add('active-btn');
+    carouselContent[id].classList.add('active');
+
+    carousel.goTo(id);
+}
+
+function directionalChange(dir){
+    var carouselBtns = document.getElementsByClassName('carousel-btn');
+    var activeLoc = 0;
+    for (var i = 0; i < carouselBtns.length; i++) {
+        if(carouselBtns[i].className.includes('active-btn')){
+            activeLoc = i;
+            break;
+        }
+    }
+
+    var gotoLoc = 0;
+    if(dir == 'next'){
+        gotoLoc = (activeLoc +1 >= carouselBtns.length ? 0 : activeLoc +1);
+    }
+    else {
+        gotoLoc = (activeLoc - 1 < 0 ? (carouselBtns.length-1) : activeLoc -1);
+    }
+
+    go(gotoLoc);
+
+}
