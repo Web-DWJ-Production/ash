7d1abf10ba4c0d56d6ba83bf30cbee8b7ff9a8d7
reddingk <kris.redding3@gmail.com>
2017-10-19 12:10:08 -0400

    add services page

--- controllers/views.controller.js
+++ controllers/views.controller.js
@@ -9,6 +9,7 @@ router.get('/', getIndex);
 router.get('/underconstruction', getUC);
 router.get('/contactus', getContactUs);
 router.get('/benefits', getBenefits);
+router.get('/services', getServices);
 router.get('/employees', expressJWT({ secret: secret }), getEmployees);
 
 module.exports = router;
@@ -16,6 +17,7 @@ module.exports = router;
 function getIndex(req, res) { res.sendFile('index.html', { root: path.join(__dirname, '../dist') }) };
 function getContactUs(req, res) { res.sendFile('contactus.html', { root: path.join(__dirname, '../dist') }) };
 function getBenefits(req, res) { res.sendFile('benefits.html', { root: path.join(__dirname, '../dist') }) };
+function getServices(req, res) { res.sendFile('services.html', { root: path.join(__dirname, '../dist') }) };
 function getUC(req, res) { res.sendFile('underconstruction.html', { root: path.join(__dirname, '../dist') }) };
 function getEmployees(req, res) {
     console.log('Access granted to employees.');
