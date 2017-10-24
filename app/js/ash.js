84a772d22a31dbc15a07d3a426c9b124db69654d
Alex Goley <ajgoley@gmail.com>
2017-10-24 00:57:24 -0400

    mail and time keeping icons

--- app/js/ash.js
+++ app/js/ash.js
@@ -61,7 +61,7 @@ function EmployeesCtrl() {
         var body = { email: email, password: password, admin: admin };
         cinnabarisland.put('/api/users/' + email, body, function (data) {
             that.getUsers();
-            alert(email + " has be succesfully updated.")
+            alert(email + " has been succesfully updated.")
         }, headers, true);
     }
 
